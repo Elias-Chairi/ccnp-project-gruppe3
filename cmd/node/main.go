a5440eb20f44493596de55745a8e3a7a9585d0ca
Elias-Chairi <eliaspc@stud.ntntu.no>
2025-10-22 11:52:19 +0200

    changed ids in example

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -13,13 +13,13 @@ func main() {
 		ID: 1,
 		Actuators: []*entity.Actuator[any]{
 			{ID: 1, Type: "HEATER", State: false},                  // off
-			{ID: 1, Type: "HEATER", Unit: "°C", State: float32(0)}, // 0 °C (no heating) (heater cannot make it colder)
-			{ID: 2, Type: "WINDOW", State: false},                  // closed
-			{ID: 2, Type: "WINDOW", State: float32(0.0)},           // 0% open
-			{ID: 3, Type: "FAN", State: false},                     // off
-			{ID: 4, Type: "FAN", Unit: "RPM", State: int32(0)},     // 0 rpm
-			{ID: 5, Type: "LIGHT", State: false},                   // off
-			{ID: 5, Type: "LIGHT", Unit: "lx", State: int32(0)},    // 0 lux
+			{ID: 2, Type: "HEATER", Unit: "°C", State: float32(0)}, // 0 °C (no heating) (heater cannot make it colder)
+			{ID: 3, Type: "WINDOW", State: false},                  // closed
+			{ID: 4, Type: "WINDOW", State: float32(0.0)},           // 0% open
+			{ID: 5, Type: "FAN", State: false},                     // off
+			{ID: 6, Type: "FAN", Unit: "RPM", State: int32(0)},     // 0 rpm
+			{ID: 7, Type: "LIGHT", State: false},                   // off
+			{ID: 8, Type: "LIGHT", Unit: "lx", State: int32(0)},    // 0 lux
 		},
 		Sensors: []*entity.Sensor[any]{
 			{ID: 1, Type: "TEMPERATURE", Unit: "°C"},
