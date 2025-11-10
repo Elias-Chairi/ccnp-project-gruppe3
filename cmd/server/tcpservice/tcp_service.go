addfe48f0f867f50aeffc49286d74c7326c9f8d6
Joakim E <joaedv97@gmail.com>
2025-11-10 11:51:17 +0100

    node id to conn map

--- cmd/server/tcpservice/tcp_service.go
+++ cmd/server/tcpservice/tcp_service.go
@@ -14,6 +14,15 @@ import (
 	"github.com/Elias-Chairi/ccnp-project-gruppe3/internal/protocol/messages"
 )
 
+// Make map to assign node IDs to the net.Conn
+var nodeIDs = make(map[int16]net.Conn)
+
+func createNodeID(conn net.Conn) int16 {
+	nodeID := int16(len(nodeIDs) + 1)
+	nodeIDs[nodeID] = conn
+	return nodeID
+}
+
 var tcpServiceAddress = net.TCPAddr{
 	// localhost address
 	IP: net.ParseIP("127.0.0.1"),
@@ -103,7 +112,20 @@ func handleRegistration(conn net.Conn) error {
 		if err != nil {
 			return fmt.Errorf("error decoding register node message %w", err)
 		}
-		// todo: reply with assigned node ID
+
+		// Create and assign new node ID
+		nodeID := createNodeID(conn)
+
+		// Send ACK_SUCCESS response with the assigned node ID
+		response := messages.AckErrorMessage{
+			Code: constants.ACK_SUCCESS,
+			Data: fmt.Sprintf("NodeID:%d", nodeID),
+		}
+
+		if err := sendResponse(conn, response); err != nil {
+			return fmt.Errorf("failed to send node ID response: %w", err)
+		}
+
 		// todo: send new node to control panel(s)
 		return handleConn(conn, handleNode)
 	case uint8(constants.REGISTER_CONTROL):
