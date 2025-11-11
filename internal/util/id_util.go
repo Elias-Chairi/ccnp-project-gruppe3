a65de31d3d2533dd38a031ee7aabe2c80e3feb89
Joakim E <joaedv97@gmail.com>
2025-11-11 14:37:48 +0100

    changed comment

--- internal/util/id_util.go
+++ internal/util/id_util.go
@@ -1,9 +1,7 @@
 package util
 
-import "net"
-
 // GetUniqueID generates a unique uint8 ID not present in the existingIDs map.
-func GetUniqueID(existingIDs map[uint8]net.Conn) uint8 {
+func GetUniqueID(existingIDs map[uint8]any) uint8 {
 	newID := uint8(len(existingIDs) + 1)
 	for {
 		if _, exists := existingIDs[newID]; !exists {
