8a6f90e58d73089271ca4e09db915c51e8944dc4
Joakim E <joaedv97@gmail.com>
2025-11-11 14:27:28 +0100

    removed uint8 from newID

--- internal/util/id_util.go
+++ internal/util/id_util.go
@@ -5,7 +5,7 @@ import "net"
 // Generates a unique ID based on the current number of existing IDs.
 // Is a id generator that maps the created ID to the net.Conn to be implemented by node and control panel entities.
 func GetUniqueID(existingIDs map[uint8]net.Conn) uint8 {
-	var newID uint8 = uint8(len(existingIDs) + 1)
+	newID := uint8(len(existingIDs) + 1)
 	for {
 		if _, exists := existingIDs[newID]; !exists {
 			break
