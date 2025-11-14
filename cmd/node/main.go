7159964c18807096a8e346f2e56e31260511c8ea
Elias-Chairi <eliaspc@stud.ntnu.no>
2025-11-14 12:11:39 +0100

    ip flag

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"time"
 
 	"github.com/Elias-Chairi/ccnp-project-gruppe3/cmd/control-panel/connectionhandler"
 	"github.com/Elias-Chairi/ccnp-project-gruppe3/cmd/node/greenhouse"
 	"github.com/Elias-Chairi/ccnp-project-gruppe3/internal/entity"
+	"github.com/Elias-Chairi/ccnp-project-gruppe3/internal/util"
 )
 
 func main() {
@@ -35,7 +37,25 @@ func main() {
 		LightLevel:  200.0,
 	}
 
-	c := connectionhandler.ConnectionHandler{}
+	var ipList util.IPList
+	flag.Var(&ipList, "server", "A comma-separated list of server IP addresses")
+	flag.Parse()
+
+	if len(ipList) == 0 {
+		log.Println("Searching for server... (search duration: 3s)")
+		var err error
+		ipList, err = util.FindServer(3 * time.Second)
+		if err != nil {
+			log.Fatalf("Error searching for servers: %v\n", err)
+		}
+		if len(ipList) == 0 {
+			log.Fatalln("No servers found, please specify server IPs manually using the -server flag.")
+		}
+	}
+
+	c := connectionhandler.ConnectionHandler{
+		ServerIPs: ipList,
+	}
 
 	log.Println("Connecting to server...")
 	c.Connect()
